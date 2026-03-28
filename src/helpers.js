1d4005f1538ee1ed95c61abc99f14cf57e91a5e3
PedroCrqs <pedrocerqueira0506@gmail.com>
2026-03-28 15:27:09 -0300

    fix(helpers): replace biased shuffle with Fisher-Yates algorithm

--- src/helpers.js
+++ src/helpers.js
@@ -7,7 +7,12 @@ function sleep(ms) {
 }
 
 function shuffle(array) {
-  return [...array].sort(() => Math.random() - 0.5);
+  const arr = [...array];
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
 }
 
 function microVary(text) {
